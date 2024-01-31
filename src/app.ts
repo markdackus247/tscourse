49f431e84e875b6c0c4184235de26b3854cc15ce
markdackus247 <79475320+markdackus247@users.noreply.github.com>
2024-01-31 20:15:54 +0100

    added the lite-server

---
+++ src/app.ts
@@ -0,0 +1,8 @@
+const name = document.getElementById("name")! as HTMLInputElement;
+const email = document.getElementById("email");
+
+function add(n1: number, n2: number) {
+    return n1 + n2;
+}
+
+console.log(add(3, 4));
