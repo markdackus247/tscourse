dc576d483932e9bf267f863aaacfab2d595860af
markdackus247 <79475320+markdackus247@users.noreply.github.com>
2024-02-01 21:24:44 +0100

    HTML Form added

--- dist/app.js
+++ dist/app.js
@@ -1,4 +1,12 @@
+const num1 = document.getElementById("number1");
+const num2 = document.getElementById("number2");
+const form = document.getElementById('calcForm');
+form.addEventListener('submit', function (event) {
+    event.preventDefault();
+    console.log(add(Number(num1), Number(num2)));
+});
 function add(n1, n2) {
     return n1 + n2;
 }
-console.log(add(3, 4));
+// console.log(num1, num2, form);
+//# sourceMappingURL=app.js.map
--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,18 @@
-const username = document.getElementById("name")! as HTMLInputElement;
-const email = document.getElementById("email")! as HTMLInputElement;
+const num1 = document.getElementById("number1")! as HTMLInputElement;
+const num2 = document.getElementById("number2")! as HTMLInputElement;
+const form = document.getElementById('calcForm')! as HTMLFormElement;
+
+
+form.addEventListener('submit', function (event) {
+    event.preventDefault();
+    console.log(add(Number(num1), Number(num2)));    
+});
+
 
 function add(n1: number, n2: number) {
     return n1 + n2;
 }
 
-console.log(add(3, 4));
+
+
+// console.log(num1, num2, form);
