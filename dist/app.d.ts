430b25eb6383cbd0f67bad7ec6f78f92f95a4195
markdackus247 <79475320+markdackus247@users.noreply.github.com>
2024-02-03 10:41:20 +0100

    tsconfig.json changed

---
+++ dist/app.d.ts
@@ -0,0 +1,3 @@
+declare const username: HTMLInputElement;
+declare const email: HTMLInputElement;
+declare function add(n1: number, n2: number): number;
--- dist/app.js
+++ dist/app.js
@@ -1,4 +1,7 @@
+const username = document.getElementById("name");
+const email = document.getElementById("email");
 function add(n1, n2) {
     return n1 + n2;
 }
 console.log(add(3, 4));
+//# sourceMappingURL=app.js.map
